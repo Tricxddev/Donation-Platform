15df37c12fe3c8ca987ad76ac76feb5579649714
unknown <gpittaz67@gmail.com>
2024-10-03 04:21:32 +0100

    schema created

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
+//dependencies
 const express =require("express")
 const dotenv =require("dotenv")
+//paths
 const dbconnect=require("./db")
+const {causeModel,organizerModel}=require("./models/users_db")
+
+//configs
 const app=express()
 
-
+dotenv.config()
+dbconnect()
 
 PORT = process.env.PORT || 5000
 
-app.listen(PORT,(req,res)=>{
+app.listen(PORT,()=>{
     console.log(`SERVER RUNNING ON PORT ,${PORT}`)
 })
-dotenv.config()
+
 app.use(express.json())
 
-dbconnect()
+app.get("/test",(req,res)=>{
+    return res.status(200).json({msg:"Welldone Tricxddev"})
+})
+
+
+app.post("/registerOrg",(req,res)=>{
+    const   {organizerName,organizerMail,organizerPassWord}=req.body
+
+})
+
+
+
+
---
+++ models/users_db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const causeSchema = new mongoose.Schema({
+    causeTitle:{type:String},
+    desc:{type:String},
+    goalAmount:{type:Number},
+    raiseAmount:{type:Number},
+    organizer:{type:String},
+    organizer_id:{type:mongoose.Types.ObjectId,ref:'organizerModel',require:true}
+})
+const causeModel = new mongoose.model("causeModel",causeSchema);
+
+const organizerSchema = new mongoose.Schema({
+    organizerName:{type:String,require:true},
+    organizerMail:{type:String,require:true},
+    organizerPassWord:{type:String,require:true,unique:true}    
+})
+
+const organizerModel = new mongoose.model("organizerModel",organizerSchema)
+
+module.exports={causeModel,organizerModel}
